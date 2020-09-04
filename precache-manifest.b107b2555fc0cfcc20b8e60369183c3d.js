4064d5790b83404d695c99bbec4fa9a1d53ca442
reginaldo melo <reginaldo.melo56@gmail.com>
2020-09-03 23:55:16 -0300

    Updates

---
+++ precache-manifest.b107b2555fc0cfcc20b8e60369183c3d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "22f0ca1cfe7ff724fad2df4cd1b64d29",
+    "url": "/.chucknorris/index.html"
+  },
+  {
+    "revision": "4e29c93422935645c685",
+    "url": "/.chucknorris/static/css/main.524e53c1.chunk.css"
+  },
+  {
+    "revision": "23868c91e40bf5918bcc",
+    "url": "/.chucknorris/static/js/2.16ee6872.chunk.js"
+  },
+  {
+    "revision": "4845593b3544c4878bbf7b7c87ce3e13",
+    "url": "/.chucknorris/static/js/2.16ee6872.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4e29c93422935645c685",
+    "url": "/.chucknorris/static/js/main.17b8fa12.chunk.js"
+  },
+  {
+    "revision": "b0bfc5ab8a6f9517e90d",
+    "url": "/.chucknorris/static/js/runtime-main.8a34fbf9.js"
+  }
+]);
